cda339ba56a41b77f78c565287430a39784ea5be
Aditya Choudhary <113711866+Aditya1101999@users.noreply.github.com>
2023-05-21 16:19:41 +0530

    added changes for #3

--- modules/initModule.py
+++ modules/initModule.py
@@ -1,5 +1,5 @@
 def initFunction():
-    # Add any initialization steps here
+    # Add any initialization steps
     print("Initializing the program...")
     # Perform initialization tasks
     print("Program initialized!")
