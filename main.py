6b1b19930e4599c45f051780391e7864155bf95e
unknown <ashuisalive@gmail.com>
2023-05-21 14:00:57 +0530

    added changes for #1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 # main file to execute all modules in gui and cli
+
+def main():
+    print("this is the main file")
+
+main()
--- modules/dateModule.py
+++ modules/dateModule.py
@@ -2,15 +2,18 @@
 
 import datetime
 
-# Get the current date and time
-current_datetime = datetime.datetime.now()
+def dateModule():
+    # Get the current date and time
+    current_datetime = datetime.datetime.now()
 
-# Format the date and time
-formatted_date = current_datetime.strftime("%B %d, %Y")
-formatted_time = current_datetime.strftime("%I:%M %p")
+    # Format the date and time
+    formatted_date = current_datetime.strftime("%B %d, %Y")
+    formatted_time = current_datetime.strftime("%I:%M %p")
 
-# Create a beautiful display string
-display_string = f"Current Date: {formatted_date}\nCurrent Time: {formatted_time}"
+    # Create a beautiful display string
+    display_string = f"Current Date: {formatted_date}\nCurrent Time: {formatted_time}"
 
-# Print the display string
-print(display_string)
+    # Print the display string
+    print(display_string)
+
+dateModule()
