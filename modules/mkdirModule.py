850826e4ca6486c81c1a2c5d9a8f6f53a95ff13d
unknown <ashuisalive@gmail.com>
2023-05-23 22:21:23 +0530

    updated readme.md, removed screenshots folder, added mkdir in main.py

--- modules/mkdirModule.py
+++ modules/mkdirModule.py
@@ -1,15 +1,15 @@
 import os
 
-# Taking path as input
-path_input = input("Enter the path: ")
+def mkdirModule():
+    
+    # Taking path as input
+    path_input = input("Enter the path: ")
 
-#checking for path location
-if '.' in path_input:
-    path = os.getcwd()
-else:
-    path = path_input
-
-
-    # using mkdir to create directory
-    os.mkdir(path)
-    print("Successfully created directory " + path)
+    #checking for path location
+    if '.' in path_input:
+        path = os.getcwd()
+    else:
+        path = path_input
+        # using mkdir to create directory
+        os.mkdir(path)
+        print("Successfully created directory " + path)
